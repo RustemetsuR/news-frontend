267b728c605f3fb73a5650104224b834b00b3f5c
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-10-24 17:38:57 +0600

    Added Provider with store. The Store is combined with newsReducer and commentsReducer. Added applyMiddlewhare and thunkMiddleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import newsReducer from './store/reducers/newsReducer';
+import commentsReducer from './store/reducers/commentsReducer';
+
+const rootReducer = combineReducers({
+  news: newsReducer,
+  comments: commentsReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
